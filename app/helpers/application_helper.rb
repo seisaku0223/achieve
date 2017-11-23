ad3b6509b18986ddca06dbc4cecf7f20584ad4fa
seisaku0223 <kdoll135npc@outlook.jp>
2017-11-23 15:27:30 +0000

    SNSログイン２度目のログイン不可エラー修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   def profile_img(user)
-    return image_tag(user.avatar, alt: user.name) if user.avatar?
+
+    if action_name == 'index'
+      return image_tag(user.avatar, alt: user.name, :class => "miniavatar") if user.avatar?
+    else
+      return image_tag(user.avatar, alt: user.name) if user.avatar?
+    end
 
     unless user.provider.blank?
       img_url = user.image_url
@@ -8,8 +13,11 @@ module ApplicationHelper
       img_url = 'no_image.png'
     end
 
-    image_tag(img_url, alt: user.name)
-
+    if action_name == 'index'
+      image_tag(img_url, alt: user.name, :class => "miniavatar")
+    else
+      image_tag(img_url, alt: user.name)
+    end
   end
 end
 
@@ -37,15 +45,6 @@ module ActionView
 
         resource.errors.include?(method) ? 'has-error' : ''
       end
-    end
-
-    def profile_img(user)
-      unless user.provider.blank?
-        img_url = user.image_url
-      else
-        img_url = 'no_image.png'
-      end
-      image_tag(img_url, alt: user.name)
     end
 
     class FormBuilder
